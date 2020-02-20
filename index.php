1437fcda899a5fccf22343ef3e6a88db95739099
Tin Hoang <minhkies@gmail.com>
2020-02-20 14:27:33 -0800

    Init

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>change.org News</title>
+    <link href="styles/styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div class="container">
+        <div class="nav">
+            <div class="logo-container">
+                <img src="./img/logo.png" alt=""Logo/>
+            </div>
+            <div class="nav-container">
+                <p class="btn-login">Login</p>
+            </div>
+        </div>
+        <div class="hero">
+            <div class="hero-content-container">
+                <p class="txt-hero large-text">The World’s Platform for Change</p>
+                <div class="btn-hero">
+                    <p>Start a Petition</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
