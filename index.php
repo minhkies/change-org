691776e4a5d6622b7b1187a3b86c798a79ae079e
naomiokada <Naomiokada17@gmail.com>
2020-02-25 17:44:43 -0800

    finished the text without img on hero

--- index.php
+++ index.php
@@ -16,14 +16,35 @@
                 <p class="btn-login">Login</p>
             </div>
         </div>
+
+
+<!--   HERO IMAGE -->
         <div class="hero">
             <div class="hero-content-container">
-                <p class="txt-hero large-text">The World’s Platform for Change</p>
-                <div class="btn-hero">
-                    <p>Start a Fundraiser</p>
+
+                <div class="left-hero">
+                    <p class="txt-hero large-text">The World’s Platform for Change</p>
+                    <div class="txt-desc-container">
+                        <div class="redline"></div>
+                        <p class="txt-desc">Starting campaigns, mobilizing supporters, and  working with decision makers to drive solutions.</p>
+                    </div>
+
+                    <div class="btn-hero">
+                        <p>Start a Fundraiser</p>
+                    </div>
                 </div>
+
+                <div class="right-hero">
+
+
+               </div>
+
             </div>
         </div>
+
+<!-- ////// END OF HERO IMAGE //////// -->
+
+
         <div class="posts-container">
             <p class="post-container-header mid-text">Latest Posts</p>
         </div>
